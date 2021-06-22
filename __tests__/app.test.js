65004870f62aefafbc34c38536cd37dce6c4d797
cmmerritt <christiane.merritt@gmail.com>
2021-06-21 23:11:20 -0700

    munge quote function passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,16 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import { formatQuote } from '../lib/utils/utils.js';
+import quoteData from '../data/quoteData.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+const testQuote = 'A theatre is the most important sort of house in the world, because that\'s where people are shown what they could be if they wanted, and what they\'d like to be if they dared to and what they really are';
+
+
+describe('quote API munging', () => {
+  it('munges quote', async () => {
+    const expected = testQuote;
+    const actual = formatQuote(quoteData);
+    expect(actual).toEqual(expected);
   });
 });
