21287b90d3c09b2cce8e875557795ed4cebc4b55
cmmerritt <christiane.merritt@gmail.com>
2021-06-21 23:49:31 -0700

    route tests pass

--- models/Quote.js
+++ models/Quote.js
@@ -16,4 +16,28 @@ export default class Quote {
 
     return new Quote(rows[0]);
   }
+
+  static async findById(id) {
+    const { rows } = await pool.query('SELECT * FROM quotes WHERE id=$1', [id]);
+
+    return new Quote(rows[0]);
+  }
+
+  static async findAll() {
+    const { rows } = await pool.query('SELECT * FROM quotes');
+
+    return rows.map(row => new Quote(row));
+  }
+
+  static async update(quote, id) {
+    const { rows } = await pool.query('UPDATE quotes SET quote = $1 WHERE id = $2 RETURNING *', [quote.quote, id]);
+
+    return new Quote(rows[0]);
+  }
+
+  static async delete(id) {
+    const { rows } = await pool.query('DELETE FROM quotes WHERE id = $1 RETURNING *', [id]);
+
+    return new Quote(rows[0]);
+  }
 }
